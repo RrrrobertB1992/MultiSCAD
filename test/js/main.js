cf6cafae36ce50a850c6097e80918de56eed2e9b
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-07-02 22:52:19 -0500

    Testing
    Added a test folder to handle tests to add new features.  I am currently testing SVGSplit and manioulating the code to be able to impliment it into MultiSCAD.

---
+++ test/js/main.js
@@ -0,0 +1,100 @@
+/* Scripts */
+
+$(document).ready(function() {
+
+  function readfiles(files) {
+      parseFile(files[0]);
+  }
+
+  // This is where the magic happens.
+  function parseFile(file) {
+    if (tests.filereader === true && acceptedTypes[file.type] === true) {
+      var reader = new FileReader();
+
+      reader.onload = function(event) {
+        holder.innerHTML = event.target.result;
+
+        var svgNode = $(holder).find("svg"),
+        result = event.target.result,
+        items = [],
+        svgTag = result.match('<svg[^>]*>'),
+        svgHead = svgTag[0],
+        svgFoot = "</svg>";
+
+        while (svgNode.children().length < 2) {
+          svgNode = svgNode.children();
+        }
+
+        for (var i = 0, length = svgNode.children().length; i < length; i++) {
+          var node = svgNode.children().eq(i)[0],
+              id = svgNode.children().eq(i).removeAttr("display").attr("id"),
+              nodeText = serializer.serializeToString(node);
+
+          nodeText = svgHead + nodeText + svgFoot;
+          zip.file(id+".svg", nodeText);
+
+          // Add file previews below if enable-preview is checked.
+          if ($('#enable-preview').is(':checked')) {
+            items[i] = "<div>"+nodeText+"</div>";
+            $("#split-svgs").append(items[i]);
+          }
+        }
+
+        content = zip.generate();
+        location.href="data:application/zip;base64," + content; // Enable for auto-download
+      };
+
+      reader.readAsText(file);
+
+    }
+  }
+
+  var holder = document.getElementById('holder'),
+      serializer = new XMLSerializer(),
+      zip = new JSZip(),
+      fileArray = [],
+      tests = {
+      filereader : typeof FileReader != 'undefined',
+      dnd : 'draggable' in document.createElement('span'),
+      formdata : !!window.FormData,
+      progress : "upload" in new XMLHttpRequest()
+  }, support = {
+      filereader : document.getElementById('filereader'),
+      formdata : document.getElementById('formdata'),
+      progress : document.getElementById('progress')
+  }, acceptedTypes = {
+      'image/svg+xml' : true
+  }, progress = document.getElementById('uploadprogress'), fileupload = document.getElementById('upload');
+
+  "filereader formdata progress".split(' ').forEach(function(api) {
+      if (tests[api] === false) {
+        support[api].className = 'fail';
+      } else {
+        support[api].className = 'hidden';
+      }
+  });
+
+  if (tests.dnd) {
+	  fileupload.querySelector('input').onchange = function() {
+      readfiles(this.files);
+    };
+    holder.ondragover = function() {
+      this.className = 'hover';
+      return false;
+    };
+    holder.ondragend = function() {
+      this.className = '';
+      return false;
+    };
+    holder.ondrop = function(e) {
+      this.className = '';
+      e.preventDefault();
+      readfiles(e.dataTransfer.files);
+    };
+  } else {
+    fileupload.className = 'hidden';
+    fileupload.querySelector('input').onchange = function() {
+      readfiles(this.files);
+    };
+  }
+});
