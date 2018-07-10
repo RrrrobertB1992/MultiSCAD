413e69f8fd93886a0676e5643e566bf660d71df3
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-07-10 18:59:03 -0500

    Merge files
    I am now using php files combine JS, and css files.  This lowers the numer of css and JS loads down to 1 each, meaning they are

---
+++ css/merged.css.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-type: text/css');
+    /* css files for combining */
+    require('bootstrap.min.css');
+    require('style.css');
+	require('all.css');
+
+
+?>
---
+++ js/merged.js.php
@@ -0,0 +1,9 @@
+
+<?php
+readfile('jquery.3.3.1.js');
+	//
+readfile('bootstrap.min.js');
+	//
+readfile('main.js');
+	//
+?>
