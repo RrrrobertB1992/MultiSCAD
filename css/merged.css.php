3099e6f71972f7f02e8c4f062eb35a9870186b16
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-07-10 20:29:51 -0500

    Shrunk css
    adding code to shrink css.

--- css/merged.css.php
+++ css/merged.css.php
@@ -1,9 +1,30 @@
 <?php
-header('Content-type: text/css');
+ob_start("compress");
+
+    function compress( $minify )
+    {
+	/* remove comments */
+    	$regex = array(
+"`^([\t\s]+)`ism"=>'',
+"`^\/\*(.+?)\*\/`ism"=>"",
+"`([\n\A;]+)\/\*(.+?)\*\/`ism"=>"$1",
+"`([\n\A;\s]+)//(.+?)[\n\r]`ism"=>"$1\n",
+"`(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+`ism"=>"\n"
+);
+$minify = preg_replace(array_keys($regex),$regex,$minify);
+
+$minify = preg_replace( '/\s*(?!<\")\/\*[^\*]+\*\/(?!\")\s*/' , '' , $minify );
+        /* remove tabs, spaces, newlines, etc. */
+    	$minify = str_replace( array("\rn", "\r", "\n", "\t", '  ', '    ', '    '), '', $minify );
+
+        return $minify;
+    }
+	
+	header('Content-type: text/css');
     /* css files for combining */
     require('bootstrap.min.css');
     require('style.css');
 	require('all.css');
 
-
+	ob_end_flush();
 ?>
