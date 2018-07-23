50338e47503e7bdaa97888c266f18010e3e65627
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-07-23 00:59:36 -0500

    Managng CSS and JS merge
    Updated CSS and JS merging php files.
    to make it a bit more organized.

--- js/merged.js.php
+++ js/merged.js.php
@@ -1,21 +1,14 @@
 <?php
-ob_start("compress");
-
-    function compress( $minify )
-    {
-		$minify = preg_replace( '/\s*(?!<\")\/\*[^\*]+\*\/(?!\")\s*/' , '' , $minify );
-		$minify = str_replace( array("\rn", "\r", "\t", '  ', '   ', '    '), '', $minify );
-        return $minify;
-    }
-
-
-header('Content-type: script/javascript');
-readfile('jquery.3.3.1.js');
-	//
-readfile('bootstrap.min.js');
-	//
-readfile('main.js');
-	//
-	
+	ob_start("compress");
+		function compress( $minify )
+		{
+			$minify = preg_replace( '/\s*(?!<\")\/\*[^\*]+\*\/(?!\")\s*/' , '' , $minify );
+			$minify = str_replace( array("\rn", "\r", "\t", '  ', '   ', '    '), '', $minify );
+			return $minify;
+		}
+		header('Content-type: script/javascript');
+		readfile('jquery.3.3.1.js');
+		readfile('bootstrap.min.js');
+		readfile('main.js');
 	ob_end_flush();
 ?>
