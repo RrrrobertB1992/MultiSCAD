99191819f3a311e8d582795d64d6aee49cb37e38
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-07-27 12:33:22 -0500

    Adjusting php loading
    Made some changes to make the website body modular.  Instead of having a set list of files to load, it loads the files dynamically.  the order can be changed by changing the leading number.

--- index.php
+++ index.php
@@ -10,31 +10,23 @@ ob_start("compress");
 
 	
 readfile('main/header.html');
-	//header.html includes all required header information as well as CSS loading
-readfile('main/logo.html');
-	//logo.html includes all the codes for the logo and header text
+	//header.html includes all required header information as well as CSS loading and logo
 readfile('main/navbar.html');
 	//navbar.html includes everything that controls the top navigation bar
-readfile('exports/_svgInput.html');
-	//_svgInput.html includes everything that controls the SVG section
-readfile('exports/_badge.html');
-	//_badge.html includes everything that controls the badge section
-readfile('exports/_bookmark.html');
-	//_bookmark.html includes everything that controls the badge section
-readfile('exports/_formCutter.html');
-	//_formCutter.html includes everything that controls the badge section
-readfile('exports/_keychain.html');
-	//_badge.html includes everything that controls the badge section
-readfile('exports/_lithophane.html');
-	//_badge.html includes everything that controls the badge section
-readfile('exports/_stamp.html');
-	//_badge.html includes everything that controls the badge section
-readfile('exports/_stencil.html');
-	//_badge.html includes everything that controls the badge section
-readfile('exports/_export.html');
-	//_badge.html includes everything that controls the badge section
+	
+
+if ($handle = opendir('exports')) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            readfile("exports/$entry");
+        }
+    }
+    closedir($handle);
+}
+	
+	
 readfile('main/footer.html');
-	//_badge.html includes everything that controls the badge section
+	//footer.html includes everything that controls the badge section
 	
 	ob_end_flush();
 ?>
