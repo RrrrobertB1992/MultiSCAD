c803a579991e5c6029b48f6906c0614934829328
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-07-28 01:26:08 -0500

    Fixed a JS bug
    Fixed a JS compression bug.  Had to remove "\n" from the str_replace as it caused the JavaScript to fail and throw and error:
    SyntaxError: unexpected token: keyword 'if'.

--- js/merged.js.php
+++ js/merged.js.php
@@ -1,11 +1,12 @@
 <?php
+//This entire file is used to combine all JavaScript files and reduce load requests down to 1 for JavaScript
 	//Used to start a buffer which allows the entire site to be loaded into memory before being processed and sent.
 	ob_start("compress");
 	//Function to remove un-needed information reducing page size and speeding up load times
 	function compress($minify)
 	{
 		$minify = preg_replace('/\s*(?!<\")\/\*[^\*]+\*\/(?!\")\s*/', '', $minify);
-		$minify = str_replace(array("\rn","\r","\n","\t",'  ','    ','    '), '', $minify);
+		$minify = str_replace(array("\rn","\r","\t",'  ','   ','    '), '', $minify);
 		return $minify;
 	}
 	//Used to set the file content type as script/javascript
@@ -14,7 +15,7 @@
 	require('jquery.js');
 	//Used to load bootstrap JavaScript
 	require('bootstrap.js');
-	//Used to load the sites Custom JavaScipt
+	//Used to load the sites custom JavaScipt, This will be split up to organise it better.
 	require('main.js');
 	//Ends the buffer and startes the compression and sends it to the browser.
 	ob_end_flush();
