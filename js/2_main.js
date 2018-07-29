eafa1e9dbf700e0ee8bd1148f3352185adb87763
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-07-28 20:53:46 -0500

    Adjusted layout system
    Instead of the old scrolling layout, the site now utalizes a tab type system.  You click the link and it hides the old section and reveal the clicked section.
    It can be reverted back to the old way by just removing 2_main.js and renaming 2_main.js.old to 2_main.js

--- js/2_main.js
+++ js/2_main.js
@@ -1,106 +1,59 @@
+( function( $ ) {
+   $( 'a[href="#"]' ).click( function(e) {
+      e.preventDefault();
+   } );
+} )( jQuery );
 
 function main() {
-
-(function () {
-	
-	var scrollLock = false;
-	
-   'use strict';
-   
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-		  $("li").removeClass("active");
-		  $(this).parent().addClass("active");
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-		  scrollLock = true;
-          if (target.length) {
-			if($(window).width() >= 1000) {
-				$('html,body').animate({
-					scrollTop: target.offset().top - $('#nav').height() + 2
-				}, 750);
-			} else if($('#nav').hasClass("affix-top")) {
-				$('html,body').animate({
-					scrollTop: target.offset().top - $('.navbar-header').height() + 5 - $('.navbar-collapse,#in').height()
-				}, 750);
-			} else {
-				$('html,body').animate({
-					scrollTop: target.offset().top - $('.navbar-header').height() + 5
-				}, 750);
+	(function() {
+		var scrollLock = false;
+		'use strict';
+		$('a.page-scroll').click(function(event) {
+			event.preventDefault();
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				$("li").removeClass("active");
+				$(this).parent().addClass("active");
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				$(".bodySection").addClass("hidden");
+				//alert(target);
+				$(target).removeClass("hidden");
 			}
-			
-			setTimeout(function() {   //calls click event after a certain time
-				scrollLock = false;
-			}, 850);
-			//$('a.page-scroll').removeClass("active");
-			
-
-            return false;
-          }
-		  
-        }
-      });
-
-	// affix the navbar after scroll below header
-	
-$('#nav').affix({
-      offset: {
-        top: $('header').height() - 2
-      }
-});	
-
-$('body').affix({
-      offset: {
-        top: $('header').height() - 2
-      }
-});	
-
-	// Hide nav on click
-  $(".navbar-nav li a").click(function (event) {
-    // check if window is small enough so dropdown is created
-    var toggle = $(".navbar-toggle").is(":visible");
-    if (toggle) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
-
-  
-  
-// Cache selectors
-var topMenu = $("#nav"),
-    topMenuHeight = topMenu.outerHeight() + 3,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
-
-// Bind to scroll
-$(window).scroll(function(){
-	
-	
-	if(scrollLock == false && $(window).scrollTop() + $(window).height() != $(document).height()) {
-   // Get container scroll position
-   var fromTop = $(this).scrollTop()+topMenuHeight;
-
-   // Get id of current scroll item
-   var cur = scrollItems.map(function(){
-     if ($(this).offset().top < fromTop)
-       return this;
-   });
-   // Get the id of the current element
-   cur = cur[cur.length-1];
-   var id = cur && cur.length ? cur[0].id : "";
-   // Set/remove active class
-   menuItems
-     .parent().removeClass("active")
-     .end().filter("[href='#"+id+"']").parent().addClass("active");
-	}
-});
-}());
-
-
+		});
+		// affix the navbar after scroll below header
+		$('#nav').affix({
+			offset: {
+				top: $('header').height() - 2
+			}
+		});
+		$('body').affix({
+			offset: {
+				top: $('header').height() - 2
+			}
+		});
+		// Hide nav on click
+		$(".navbar-nav li a").click(function(event) {
+			// check if window is small enough so dropdown is created
+			var toggle = $(".navbar-toggle").is(":visible");
+			if (toggle) {
+				$(".navbar-collapse").collapse('hide');
+			}
+		});
+		// Cache selectors
+		var topMenu = $("#nav"),
+			topMenuHeight = topMenu.outerHeight() + 3,
+			// All list items
+			menuItems = topMenu.find("a"),
+			// Anchors corresponding to menu items
+			scrollItems = menuItems.map(function() {
+				var item = $($(this).attr("href"));
+				if (item.length) {
+					return item;
+				}
+			});
+		// Bind to scroll
+		$(window).scroll(function() {
+		});
+	}());
 }
 main();
