0cef3dfc52933a065a5b8fc15fa15f9bf7fe2c25
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-07-28 22:04:37 -0500

    Cleaned up 2_main.js
    Cleaned up old codes from 2_main.js.
    Removed un-used codes, and re-organized the codes to make them cleaner.

--- js/2_main.js
+++ js/2_main.js
@@ -6,53 +6,28 @@
 
 function main() {
 	(function() {
-		var scrollLock = false;
 		'use strict';
 		$('a.page-scroll').click(function(event) {
 			event.preventDefault();
-			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				$("li").removeClass("active");
-				$(this).parent().addClass("active");
-				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-				$(".bodySection").addClass("hidden");
-				//alert(target);
-				$(target).removeClass("hidden");
-			}
+			var target = $(this.hash).length ? $(this.hash) : $('[name=' + this.hash.slice(1) + ']');
+			$("li").removeClass("active");
+			$(this).parent().addClass("active");
+			$(".bodySection").addClass("hidden");
+			$(target).removeClass("hidden");
 		});
 		// affix the navbar after scroll below header
 		$('#nav').affix({
-			offset: {
-				top: $('header').height() - 2
-			}
+			offset: { top: $('header').height() - 2 }
 		});
 		$('body').affix({
-			offset: {
-				top: $('header').height() - 2
-			}
+			offset: { top: $('header').height() - 2 }
 		});
 		// Hide nav on click
 		$(".navbar-nav li a").click(function(event) {
 			// check if window is small enough so dropdown is created
-			var toggle = $(".navbar-toggle").is(":visible");
-			if (toggle) {
+			if ($(".navbar-toggle").is(":visible")) {
 				$(".navbar-collapse").collapse('hide');
 			}
-		});
-		// Cache selectors
-		var topMenu = $("#nav"),
-			topMenuHeight = topMenu.outerHeight() + 3,
-			// All list items
-			menuItems = topMenu.find("a"),
-			// Anchors corresponding to menu items
-			scrollItems = menuItems.map(function() {
-				var item = $($(this).attr("href"));
-				if (item.length) {
-					return item;
-				}
-			});
-		// Bind to scroll
-		$(window).scroll(function() {
 		});
 	}());
 }
