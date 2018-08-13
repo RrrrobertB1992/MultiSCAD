3d401afb2dbdd0b426223d774313f1234aff4d33
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-08-13 14:11:18 -0500

    Added 401 error page

---
+++ errDocs/401.php
@@ -0,0 +1,10 @@
+<head>
+<link rel="stylesheet"	href="/errDocs/errors.css">
+</head>
+<div class="overlay"></div>
+<div class="terminal">
+	<h1>Error <span class="errorcode">401</span></h1>
+	<p class="output">Access is denied due to invalid credentials.</p>
+	<p class="output">You do not have permission to view this directory or document using the credentials that you supplied.</p>
+	<p class="output">Either check your credentials, <a href="../index.php">go home</a>, or <a href="mailto:rrrrobertb1992@gmail.com">Request assistance</a>.</p>
+</div>
