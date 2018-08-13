be92e30bf976d8a31db6dec9ac38ba07f30cf933
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-08-13 14:03:13 -0500

    Added 400 error

---
+++ errDocs/400.php
@@ -0,0 +1,9 @@
+<head>
+<link rel="stylesheet"	href="/errDocs/errors.css">
+</head>
+<div class="overlay"></div>
+<div class="terminal">
+  <h1>Error <span class="errorcode">400</span></h1>
+  <p class="output">Your client has issued a malformed or illegal request.</p>
+  <p class="output">Either check the URL, <a href="../index.php">go home</a>, or feel free to <a href="mailto:rrrrobertb1992@gmail.com">Request assistance</a>.</p>
+</div>
