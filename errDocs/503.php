7102d2bb98b4d110b3726407258edd8908787379
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-08-13 16:34:28 -0500

    Added error 503

---
+++ errDocs/503.php
@@ -0,0 +1,10 @@
+<head>
+<link rel="stylesheet"	href="/errDocs/errors.css">
+</head>
+<div class="overlay"></div>
+<div class="terminal">
+	<h1>Error <span class="errorcode">503</span></h1>
+	<p class="output">service unavailable</p>
+	<p class="output">This webpage is currently unavailable.  It may be overloaded or down for maintenance.</p>
+	<p class="output">You can <a href="<?php echo $_SERVER["REQUEST_URI"]; ?>">Reload</a> this webpage or <a href="mailto:rrrrobertb1992@gmail.com">Request assistance</a> if this issue continues.</p>
+</div>
