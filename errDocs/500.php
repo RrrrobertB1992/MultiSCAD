ab6c5b45e3baea9fb575f776cbf4a6d977bef324
RrrrobertB1992 <40670302+RrrrobertB1992@users.noreply.github.com>
2018-08-13 15:27:14 -0500

    Added 500 error

---
+++ errDocs/500.php
@@ -0,0 +1,10 @@
+<head>
+<link rel="stylesheet"	href="/errDocs/errors.css">
+</head>
+<div class="overlay"></div>
+<div class="terminal">
+	<h1>Error <span class="errorcode">500</span></h1>
+	<p class="output">Internal server error</p>
+	<p class="output">the server encountered an internal error or misconfiguration.</p>
+	<p class="output">You can <a href="mailto:rrrrobertb1992@gmail.com">Request assistance</a> if this issue continues.</p>
+</div>
